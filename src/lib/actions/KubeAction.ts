c8914605dbe002c43c0caf415ab14be1fb643624
Tristan D'audi <tristan.daudibert@gmail.com>
2022-11-02 15:21:19 +0100

    fix label creation which was not working using '/'. So i turned it to '_'

--- src/lib/actions/KubeAction.ts
+++ src/lib/actions/KubeAction.ts
@@ -23,20 +23,33 @@ export class KubeAction {
         await this.utils.spawnAndDisplay(`${this.kubectlCommand} apply -f=${filePath}`);
     }
 
+    public async stop(namespace: string, serviceName: string, labelName: string, labelValue: string) : Promise<void> {
+        const depType = await this.getDeploymentType(namespace, serviceName);
+        await this.utils.spawnAndDisplay(`${this.kubectlCommand} scale ${depType} --namespace ${namespace} ` +
+            `-l=${Constants.clusterName}_${labelName}=${labelValue} --replicas=0`);
+    }
+
+    public async start(namespace: string, serviceName: string, labelName: string, labelValue: string) : Promise<void> {
+        const depType = await this.getDeploymentType(namespace, serviceName);
+        await this.utils.spawnAndDisplay(`${this.kubectlCommand} scale ${depType} --namespace ${namespace} ` +
+            `-l=${Constants.clusterName}_${labelName}=${labelValue} --replicas=1`);
+    }
+
     public async addEasyKubeLabels(namespace: string, serviceName: string, moduleName: string = ''): Promise<void> {
         const depType = await this.getDeploymentType(namespace, serviceName);
         await this.utils.execAndReturn(`${this.kubectlCommand} label ${depType} ` +
-            `--namespace ${namespace} ${serviceName} ${Constants.clusterName}/service=${serviceName}`);
+            `--namespace ${namespace} ${serviceName} ${Constants.clusterName}_service=${serviceName}`);
         if (moduleName !== ''){
             await this.utils.execAndReturn(`${this.kubectlCommand} label ${depType} `+
-            `--namespace ${namespace} ${serviceName} ${Constants.clusterName}/module=${moduleName}`);
+            `--namespace ${namespace} ${serviceName} ${Constants.clusterName}_module=${moduleName}`);
         }
     }
 
     private async getDeploymentType(namespace: string, serviceName: string): Promise<string> {
         for (const depType of ['deployments', 'statefulsets', 'daemonsets']){
             try {
-                await this.utils.execAndReturn(`${this.kubectlCommand} get ${depType} --namespace ${namespace} ${serviceName}`);
+                await this.utils.execAndReturn(`${this.kubectlCommand} get ${depType} --namespace ${namespace} ` +
+                `--no-headers=true --output=name ${serviceName}`);
                 return depType;
             } catch (e){
                 if (! e.message.match(`"${serviceName}" not found`)) {
